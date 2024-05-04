d3e3ba84228e44ca4c2c5c4c2e68a9ab38e405b5
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-05-05 00:45:31 +0300

    Resources refactoring

---
+++ Tracker/Resources/Resources.swift
@@ -0,0 +1,21 @@
+//
+//  Resources.swift
+//  Tracker
+//
+//  Created by Александр Плешаков on 04.05.2024.
+//
+
+import UIKit
+
+enum Resources {
+    
+    enum Images {
+        static let tracksTab = UIImage(named: "TabTracks") ?? UIImage()
+        static let statisticTab = UIImage(named: "TabStatistic") ?? UIImage()
+    }
+    
+    enum Colors {
+        static let black = UIColor(named: "TBlack")
+        static let white = UIColor(named: "TWhite")
+    }
+}
