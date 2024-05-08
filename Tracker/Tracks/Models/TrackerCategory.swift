76c76cbfbee1d5ddff87c58264797dae20600063
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-05-08 20:41:32 +0300

    Tracker category model implementation

---
+++ Tracker/Tracks/Models/TrackerCategory.swift
@@ -0,0 +1,13 @@
+//
+//  TrackerCategory.swift
+//  Tracker
+//
+//  Created by Александр Плешаков on 08.05.2024.
+//
+
+import Foundation
+
+struct TrackerCategory {
+    let title: String
+    let trackers: [Tracker]
+}
