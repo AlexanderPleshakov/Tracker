a32c80a018d6c73ec1252d5b01c133ec5d440c62
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-05-10 17:18:07 +0300

    Implementation of helper for habit and event table view

---
+++ Tracker/TrackerCreation/Helpers/HabitAndEventTableViewHelper.swift
@@ -0,0 +1,89 @@
+//
+//  HabitAndEventTableViewHelper.swift
+//  Tracker
+//
+//  Created by Александр Плешаков on 10.05.2024.
+//
+
+import UIKit
+
+final class HabitAndEventTableViewHelper: NSObject {
+    let numbersOfRows: [Int]
+    
+    init(numbersOfRows: [Int] = [1, 2]) {
+        self.numbersOfRows = numbersOfRows.count == 2 ? numbersOfRows : [1, 2]
+    }
+}
+
+
+extension HabitAndEventTableViewHelper: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 75
+    }
+    
+    public func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return nil
+    }
+
+    public func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return nil
+    }
+
+    public func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return .leastNonzeroMagnitude
+    }
+
+    public func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 24
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension HabitAndEventTableViewHelper: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == 0 ? numbersOfRows[0] : numbersOfRows[1]
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: InputFieldTableViewCell.reuseIdentifier, for: indexPath)
+            
+            guard let cell = cell as? InputFieldTableViewCell else {
+                return UITableViewCell()
+            }
+            
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "DefaultCell", for: indexPath)
+            let cellText: String
+            if numbersOfRows[1] == 1 {
+                cellText = "Расписание"
+            } else {
+                cellText = indexPath.row == 0 ? "Категория" : "Расписание"
+            }
+            
+            cell.backgroundColor = Resources.Colors.cellBackground
+            cell.accessoryType = .disclosureIndicator
+            
+            
+            if #available(iOS 14.0, *) {
+                var content = cell.defaultContentConfiguration()
+                content.attributedText = NSAttributedString(string: cellText, attributes: [.font: UIFont.systemFont(ofSize: 17, weight: .regular)])
+                cell.contentConfiguration = content
+            } else {
+                cell.textLabel?.text = cellText
+                cell.textLabel?.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+            }
+            
+            
+            return cell
+        }
+    }
+}
--- Tracker/TrackerCreation/NewHabitViewController.swift
+++ Tracker/TrackerCreation/NewHabitViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 final class NewHabitViewController: UIViewController {
     // MARK: Properties
     
+    private let tableViewHelper = HabitAndEventTableViewHelper()
+    
     private let tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .insetGrouped)
         tableView.register(InputFieldTableViewCell.self, forCellReuseIdentifier: InputFieldTableViewCell.reuseIdentifier)
@@ -31,8 +33,10 @@ final class NewHabitViewController: UIViewController {
     
     private func configure() {
         view.backgroundColor = Resources.Colors.white
-        tableView.dataSource = self
-        tableView.delegate = self
+        
+        tableView.dataSource = tableViewHelper
+        tableView.delegate = tableViewHelper
+        
         tableView.backgroundColor = Resources.Colors.white
         
         title = "Новая привычка"
@@ -52,71 +56,5 @@ final class NewHabitViewController: UIViewController {
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
-    }
-}
-
-extension NewHabitViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return section == 0 ? 1 : 2
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: InputFieldTableViewCell.reuseIdentifier, for: indexPath)
-            
-            guard let cell = cell as? InputFieldTableViewCell else {
-                return UITableViewCell()
-            }
-            
-            return cell
-        } else {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "DefaultCell", for: indexPath)
-            let cellText = indexPath.row == 0 ? "Категория" : "Расписание"
-            cell.backgroundColor = Resources.Colors.cellBackground
-            cell.accessoryType = .disclosureIndicator
-            
-            
-            if #available(iOS 14.0, *) {
-                var content = cell.defaultContentConfiguration()
-                content.attributedText = NSAttributedString(string: cellText, attributes: [.font: UIFont.systemFont(ofSize: 17, weight: .regular)])
-                cell.contentConfiguration = content
-            } else {
-                cell.textLabel?.text = cellText
-                cell.textLabel?.font = UIFont.systemFont(ofSize: 17, weight: .regular)
-            }
-            
-            
-            return cell
-        }
-    }
-}
-
-extension NewHabitViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 75
-    }
-    
-    public func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        return nil
-    }
-
-    public func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        return nil
-    }
-
-    public func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return .leastNonzeroMagnitude
-    }
-
-    public func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 24
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
