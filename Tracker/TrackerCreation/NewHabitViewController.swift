bcdd162827e25eb9857ed9f52bff73a221ade72a
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-05-10 16:49:00 +0300

    Category and timetable cells implementation

--- Tracker/TrackerCreation/NewHabitViewController.swift
+++ Tracker/TrackerCreation/NewHabitViewController.swift
@@ -13,6 +13,7 @@ final class NewHabitViewController: UIViewController {
     private let tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .insetGrouped)
         tableView.register(InputFieldTableViewCell.self, forCellReuseIdentifier: InputFieldTableViewCell.reuseIdentifier)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "DefaultCell")
         tableView.translatesAutoresizingMaskIntoConstraints = false
         
         return tableView
@@ -56,22 +57,66 @@ final class NewHabitViewController: UIViewController {
 
 extension NewHabitViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return section == 0 ? 1 : 2
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: InputFieldTableViewCell.reuseIdentifier, for: indexPath)
-        
-        guard let cell = cell as? InputFieldTableViewCell else {
-            return UITableViewCell()
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: InputFieldTableViewCell.reuseIdentifier, for: indexPath)
+            
+            guard let cell = cell as? InputFieldTableViewCell else {
+                return UITableViewCell()
+            }
+            
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "DefaultCell", for: indexPath)
+            let cellText = indexPath.row == 0 ? "Категория" : "Расписание"
+            cell.backgroundColor = Resources.Colors.cellBackground
+            cell.accessoryType = .disclosureIndicator
+            
+            
+            if #available(iOS 14.0, *) {
+                var content = cell.defaultContentConfiguration()
+                content.attributedText = NSAttributedString(string: cellText, attributes: [.font: UIFont.systemFont(ofSize: 17, weight: .regular)])
+                cell.contentConfiguration = content
+            } else {
+                cell.textLabel?.text = cellText
+                cell.textLabel?.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+            }
+            
+            
+            return cell
         }
-        
-        return cell
     }
 }
 
 extension NewHabitViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 75
+    }
+    
+    public func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return nil
+    }
+
+    public func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return nil
+    }
+
+    public func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return .leastNonzeroMagnitude
+    }
+
+    public func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 24
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
