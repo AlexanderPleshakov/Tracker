5ad1334b7eb8e32a98a288207f677c78a3a67ca8
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-05-11 17:39:54 +0300

    Implementation of presentation selected days in subtitle

--- Tracker/Models/Day.swift
+++ Tracker/Models/Day.swift
@@ -7,7 +7,12 @@
 
 import Foundation
 
-enum Day {
-    case everyday
-    case monday, tuesday, wednesday, thursday, friday, saturday, sunday
+enum Day: String {
+    case monday = "Пн"
+    case tuesday = "Вт"
+    case wednesday = "Ср"
+    case thursday = "Чт"
+    case friday = "Пт"
+    case saturday = "Сб"
+    case sunday = "Вс"
 }
--- Tracker/TrackerCreation/Views/DisclosureTableViewCell.swift
+++ Tracker/TrackerCreation/Views/DisclosureTableViewCell.swift
@@ -12,9 +12,33 @@ final class DisclosureTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        
+        NotificationCenter.default.addObserver(forName: TimetableViewController.buttonTappedNotification, object: nil, queue: .main) { [weak self] notification in
+            if let days = notification.userInfo?["days"] as? [Day] {
+                self?.changeSubtitle(days: days)
+            }
+            print("Notification")
+        }
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func changeSubtitle(days: [Day]) {
+        if textLabel?.text == "Расписание" {
+            if days.count == 7 {
+                detailTextLabel?.text = "Каждый день"
+                return
+            }
+            var values = [String]()
+            
+            for day in days {
+                values.append(day.rawValue)
+            }
+            let text = values.joined(separator: ", ")
+            
+            detailTextLabel?.text = text
+        }
     }
 }
