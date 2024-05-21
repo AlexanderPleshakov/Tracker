e6cc6dc9efead49f38ec619740d2e3cb9ecfa280
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-05-21 23:47:58 +0300

    Added params model for trackers collection

---
+++ Tracker/Trackers/Views/TrackersCollectionView/Models/GeometricParams.swift
@@ -0,0 +1,24 @@
+//
+//  GeometricParams.swift
+//  Tracker
+//
+//  Created by Александр Плешаков on 21.05.2024.
+//
+
+import Foundation
+
+struct GeometricParams {
+    let cellCount: Int
+    let leftInset: CGFloat
+    let rightInset: CGFloat
+    let cellSpacing: CGFloat
+    let paddingWidth: CGFloat
+    
+    init(cellCount: Int, leftInset: CGFloat, rightInset: CGFloat, cellSpacing: CGFloat) {
+        self.cellCount = cellCount
+        self.leftInset = leftInset
+        self.rightInset = rightInset
+        self.cellSpacing = cellSpacing
+        self.paddingWidth = leftInset + rightInset + CGFloat(cellCount - 1) * cellSpacing
+    }
+}
