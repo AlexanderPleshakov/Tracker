228971e192212c1463ac0fddc829b248e60ef83c
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-05-24 12:50:17 +0300

    Custom TextField class was moved to the own file

--- Tracker/TrackerCreation/Controllers/NewCategoryViewController.swift
+++ Tracker/TrackerCreation/Controllers/NewCategoryViewController.swift
@@ -22,25 +22,6 @@ final class NewCategoryViewController: UIViewController {
         return button
     }()
     
-    class TextFieldWithPadding: UITextField {
-        var textPadding = UIEdgeInsets(
-            top: 0,
-            left: 16,
-            bottom: 0,
-            right: 16
-        )
-
-        override func textRect(forBounds bounds: CGRect) -> CGRect {
-            let rect = super.textRect(forBounds: bounds)
-            return rect.inset(by: textPadding)
-        }
-
-        override func editingRect(forBounds bounds: CGRect) -> CGRect {
-            let rect = super.editingRect(forBounds: bounds)
-            return rect.inset(by: textPadding)
-        }
-    }
-    
     private let textField: UITextField = {
         let textField = TextFieldWithPadding()
         textField.placeholder = "Введите название категории"
