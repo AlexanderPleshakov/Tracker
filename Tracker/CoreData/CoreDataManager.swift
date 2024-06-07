ccf938f275679de57e83c5ae9649618b31abdc66
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-06-07 21:35:56 +0300

    Added CoreData manager

---
+++ Tracker/CoreData/CoreDataManager.swift
@@ -0,0 +1,38 @@
+//
+//  CoreDataManager.swift
+//  Tracker
+//
+//  Created by Александр Плешаков on 07.06.2024.
+//
+
+import UIKit
+import CoreData
+
+final class CoreDataManager {
+    static let shared = CoreDataManager()
+    private init() {}
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Data")
+        container.loadPersistentStores { description, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+                print("Context saved")
+            } catch {
+                context.rollback()
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
