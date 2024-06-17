e7057c96ff54dbd0ad9138ac6e7c1e3cb7c3ac35
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-06-17 18:30:59 +0300

    Added screenshot tests for the dark theme

--- TrackerTests/TrackerTests.swift
+++ TrackerTests/TrackerTests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 final class TrackerTests: XCTestCase {
     
+    // MARK: Light Theme
+    
     // MARK: Main screen tests
 
     func testTrackersScreenLight() throws {
@@ -87,5 +89,83 @@ final class TrackerTests: XCTestCase {
         let vc = NewCategoryViewController(viewModel: CategoriesViewModelMock())
         
         assertSnapshots(of: vc, as: [.image(traits: .init(userInterfaceStyle: .light))])
+    }
+    
+    // MARK: Dark Theme
+    
+    func testTrackersScreenDark() throws {
+        let vc = TrackersViewController()
+        
+        assertSnapshots(of: vc, as: [.image(traits: .init(userInterfaceStyle: .dark))])
+    }
+    
+    func testTrackersNavBarDark() throws {
+        let vc = TrackersViewController()
+        let nc = TrackersNavigationController(rootViewController: vc)
+        
+        assertSnapshots(of: nc, as: [.image(traits: .init(userInterfaceStyle: .dark))])
+    }
+    
+    func testTabBarDark() throws {
+        let tb = TabBarViewController()
+        
+        assertSnapshots(of: tb, as: [.image(traits: .init(userInterfaceStyle: .dark))])
+    }
+    
+    func testTrackersCollectionNoCompletedCellDark() throws {
+        let cell = TrackersCollectionViewCell(frame: CGRect(x: 0, y: 0, width: 167, height: 132))
+        let tracker = Tracker(id: UUID(), name: "Test", color: 0x46E69D, emoji: "👽", timetable: [.monday], creationDate: Date())
+        cell.configure(tracker: tracker, isCompleted: false, completedDays: 3, date: Date())
+        
+        assertSnapshots(of: cell, as: [.image(traits: .init(userInterfaceStyle: .dark))])
+    }
+    
+    func testTrackersCollectionCompletedCellDark() throws {
+        let cell = TrackersCollectionViewCell(frame: CGRect(x: 0, y: 0, width: 167, height: 132))
+        let tracker = Tracker(id: UUID(), name: "Test", color: 0x46E69D, emoji: "👽", timetable: [.monday], creationDate: Date())
+        cell.configure(tracker: tracker, isCompleted: true, completedDays: 3, date: Date())
+        
+        assertSnapshots(of: cell, as: [.image(traits: .init(userInterfaceStyle: .dark))])
+    }
+    
+    // MARK: Tracker creation tests
+    
+    func testNewTrackerScreenDark() throws {
+        let vc = NewTrackerViewController(currentDate: Date())
+        
+        assertSnapshots(of: vc, as: [.image(traits: .init(userInterfaceStyle: .dark))])
+    }
+    
+    func testNewHabitScreenDark() throws {
+        let viewModel = NewTrackerViewModel(type: .habit, date: Date())
+        let vc = NewHabitOrEventViewController(viewModel: viewModel)
+        
+        assertSnapshots(of: vc, as: [.image(traits: .init(userInterfaceStyle: .dark))])
+    }
+    
+    func testNewEventScreenDark() throws {
+        let viewModel = NewTrackerViewModel(type: .event, date: Date())
+        let vc = NewHabitOrEventViewController(viewModel: viewModel)
+        
+        assertSnapshots(of: vc, as: [.image(traits: .init(userInterfaceStyle: .dark))])
+    }
+    
+    func testTimetableScreenDark() throws {
+        let vc = TimetableViewController(viewModel: TimetableViewModel())
+        
+        assertSnapshots(of: vc, as: [.image(traits: .init(userInterfaceStyle: .dark))])
+    }
+    
+    func testCategoriesScreenDark() throws {
+        let vc = CategoriesViewController(viewModel: CategoriesViewModelMock())
+        
+        assertSnapshots(of: vc, as: [.image(traits: .init(userInterfaceStyle: .dark))])
+    }
+    
+    func testNewCategoryScreenDark() throws {
+        
+        let vc = NewCategoryViewController(viewModel: CategoriesViewModelMock())
+        
+        assertSnapshots(of: vc, as: [.image(traits: .init(userInterfaceStyle: .dark))])
     }
 }
