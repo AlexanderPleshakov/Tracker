71808a48b68e0c8d514927ae23fd40e637d429d8
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-06-27 01:15:42 +0300

    Added statistic screen with stub

--- Tracker/Statistic/StatisticViewController.swift
+++ Tracker/Statistic/StatisticViewController.swift
@@ -8,10 +8,48 @@
 import UIKit
 
 final class StatisticViewController: UIViewController {
+    // MARK: Properties
+    
+    private let stub = StubView(text: "Анализировать пока нечего",
+                                image: Resources.Images.statisticStub)
+    
+    // MARK: Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureUI()
+    }
+    
+    // MARK: Methods
+}
+
+// MARK: UI
+
+extension StatisticViewController {
+    private func configureUI() {
         view.backgroundColor = Resources.Colors.background
+        title = NSLocalizedString("statistic", comment: "")
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: Resources.Colors.foreground]
+        
+        setupSubviews()
+    }
+    
+    private func setupSubviews() {
+        [stub].forEach { view in
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        setupStub()
+    }
+    
+    private func setupStub() {
+        view.addSubview(stub)
+        
+        NSLayoutConstraint.activate([
+            stub.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            stub.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
+        ])
     }
 }
