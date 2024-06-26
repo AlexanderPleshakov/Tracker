0dd78245268824583dbaa6f554d3eb2649565aac
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-06-26 15:13:36 +0300

    Completion type model was moved in the own file

--- Tracker/CoreData/Stores/TrackerStore.swift
+++ Tracker/CoreData/Stores/TrackerStore.swift
@@ -278,10 +278,3 @@ final class TrackerStore {
     }
 }
 
-
-enum CompletionType: Int {
-    case any = 0
-    case completed = 2
-    case uncompleted = 3
-}
-
---
+++ Tracker/Models/CompletionType.swift
@@ -0,0 +1,14 @@
+//
+//  CompletionType.swift
+//  Tracker
+//
+//  Created by Александр Плешаков on 26.06.2024.
+//
+
+import Foundation
+
+enum CompletionType: Int {
+    case any = 0
+    case completed = 2
+    case uncompleted = 3
+}
