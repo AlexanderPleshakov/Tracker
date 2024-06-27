2b9e7fa047948a99cbd73075b91f321583e83466
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-06-27 14:47:03 +0300

    Added gradient cell for statistic and added table view

--- Tracker/Statistic/StatisticViewController.swift
+++ Tracker/Statistic/StatisticViewController.swift
@@ -13,6 +13,15 @@ final class StatisticViewController: UIViewController {
     private let stub = StubView(text: "Анализировать пока нечего",
                                 image: Resources.Images.statisticStub)
     
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.register(StatisticTableViewCell.self,
+                           forCellReuseIdentifier: StatisticTableViewCell.reuseIdentifier)
+        tableView.backgroundColor = Resources.Colors.background
+        
+        return tableView
+    }()
+    
     // MARK: Life Cycle
     
     override func viewDidLoad() {
@@ -22,6 +31,56 @@ final class StatisticViewController: UIViewController {
     }
     
     // MARK: Methods
+}
+
+// MARK: UITableViewDataSource
+
+extension StatisticViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: StatisticTableViewCell.reuseIdentifier,
+            for: indexPath) as? StatisticTableViewCell
+        else {
+            return UITableViewCell()
+        }
+        
+        cell.setTitle(text: "Трекеров завершено")
+        cell.setCount(count: 5)
+        
+        return cell
+    }
+}
+
+// MARK: UITableViewDelegate
+
+extension StatisticViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
+    
+    public func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return nil
+    }
+    
+    public func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return nil
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return .leastNonzeroMagnitude
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return .leastNonzeroMagnitude
+    }
 }
 
 // MARK: UI
@@ -34,14 +93,18 @@ extension StatisticViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: Resources.Colors.foreground]
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         setupSubviews()
     }
     
     private func setupSubviews() {
-        [stub].forEach { view in
+        [stub, tableView].forEach { view in
             view.translatesAutoresizingMaskIntoConstraints = false
         }
-        setupStub()
+        //setupStub()
+        setupTable()
     }
     
     private func setupStub() {
@@ -50,6 +113,17 @@ extension StatisticViewController {
         NSLayoutConstraint.activate([
             stub.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
             stub.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
+        ])
+    }
+    
+    private func setupTable() {
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 24),
         ])
     }
 }
